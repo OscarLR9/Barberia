5b11e3c709e1469c43bcc0793d194266ddf3f981
OscarLR9 <oscar.lopezr@uanl.edu.mx>
2025-03-31 21:20:27 -0600

    Update conexion.php Oscar
    Cambie para que el usuario introduciera su zona horaria y corregirel problema con las fechas

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,13 @@
 <?php
+date_default_timezone_set('America/Mexico_City'); // Configura la zona horaria
+
 $conexion = mysqli_connect("localhost", "root", "", "db_barberia");
 
 if (!$conexion) {
-    // Si la conexión falla, mostramos el error
     echo 'La conexión falló: ' . mysqli_connect_error();
 }
+
+// Asegurar que MySQL use la misma zona horaria
+mysqli_query($conexion, "SET time_zone = '-06:00';"); // Ajusta según tu zona horaria
+?>
+
